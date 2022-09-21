b2a0bbfc6133fb12d68b44d77028a6cee6b8f44a
cavalenciad <cavalenciad@outlook.com>
2022-09-21 18:40:08 -0500

    Reconfiguración start

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 const session = require ('express-session');
 const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 const cookies = require ('cookie-parser');
-const {PORT} = require('./config.js');
+const PORT = process.env.PORT || 3050
 
 const rutaMain = require("./routers/main.js");
 const rutaUser = require("./routers/users.js");
