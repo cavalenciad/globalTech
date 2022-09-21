6e74230fc06740994ac92456531fdcaebbacbabc
cavalenciad <cavalenciad@outlook.com>
2022-09-21 18:03:06 -0500

    Configuración despliegue

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const methodOverride = require('method-override');
 const session = require ('express-session');
 const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 const cookies = require ('cookie-parser');
-/* const pruebaDb = require ("./routers/pruebaDB.js"); 
-const pruebaUserDB = require("./routers/pruebaUserDB.js") */
+import {PORT} from './config.js'
 
 const rutaMain = require("./routers/main.js");
 const rutaUser = require("./routers/users.js");
@@ -38,6 +37,6 @@ app.use("/", pruebaUserDB);  */
 
 app.use(express.static("./public"));
 
-app.listen(3050, () =>
+app.listen(PORT, () =>
    console.log("Servidor activo en el puerto 3050")
 );   
