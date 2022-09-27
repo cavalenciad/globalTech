b21f35c784b1cc2b15c046cedb4def5f5c7c086a
cavalenciad <cavalenciad@outlook.com>
2022-09-27 18:05:45 -0500

    Segunda actualización del config

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     "username": "sql10522671",
     "password": "QbtIqexaNA",
     "database": "sql10522671",
-    "host": "http://sql10.freemysqlhosting.net/",
+    "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql",
     "logging": sql => sql.substr(0,20)
   },
@@ -11,14 +11,14 @@ module.exports = {
     "username": "sql10522671",
     "password": "QbtIqexaNA",
     "database": "sql10522671",
-    "host": "http://sql10.freemysqlhosting.net/",
+    "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   },
   "production": {
     "username": "sql10522671",
     "password": "QbtIqexaNA",
     "database": "sql10522671",
-    "host": "http://sql10.freemysqlhosting.net/",
+    "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   }
 }
