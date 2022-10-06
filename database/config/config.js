e9fd7f9b2c3163691d6aff5f00069cd9c8fd9622
cavalenciad <cavalenciad@outlook.com>
2022-10-05 21:26:48 -0500

    Update config

--- database/config/config.js
+++ database/config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   "development": {
-    "username": "sql10522671",
-    "password": "QbtIqexaNA",
-    "database": "sql10522671",
-    "host": "sql10.freemysqlhosting.net",
+    "username": "root",
+    "password": "Juanma.1991",
+    "database": "db_globaltech",
+    "host": "localhost",
     "dialect": "mysql",
     "logging": sql => sql.substr(0,20)
   },
   "test": {
-    "username": "sql10522671",
-    "password": "QbtIqexaNA",
-    "database": "sql10522671",
-    "host": "sql10.freemysqlhosting.net",
+    "username": "root",
+    "password": "Juanma.1991",
+    "database": "db_globaltech",
+    "host": "localhost",
     "dialect": "mysql"
   },
   "production": {
-    "username": "sql10522671",
-    "password": "QbtIqexaNA",
-    "database": "sql10522671",
-    "host": "sql10.freemysqlhosting.net",
+    "username": "root",
+    "password": "Juanma.1991",
+    "database": "db_globaltech",
+    "host": "localhost",
     "dialect": "mysql"
   }
 }
